1248b74ee25dbed3b1f43a1bc88c9f3a137f4323
mohd zubair <mdzubair3782@gmail.com>
2022-11-24 13:47:58 +0530

    Expenseitem

---
+++ src/Component/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ExpenseItem = () => {
+  return (
+    <div>
+    <div><h2>ExpenseItem !</h2>
+    <h3>Food Rs 10</h3>
+    <h3>petrol Rs 100</h3>
+    <h3>movies Rs 200</h3>
+    </div>
+    </div>
+  )
+}
+
+export default ExpenseItem
